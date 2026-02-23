aed2918a479099f3da74783f6031f915842d15f0
omikayelyan <104993462+omikayelyan@users.noreply.github.com>
2026-02-23 20:13:35 +0400

    Update augmentation.py

--- src/augmentation.py
+++ src/augmentation.py
@@ -1,28 +1,39 @@
 import pandas as pd
 from sklearn.preprocessing import StandardScaler
 
-
-def augment_with_scaled_data(X_train: pd.DataFrame, y_train: pd.Series):
+def augmentation_data(X_train: pd.DataFrame, y_train: pd.Series):
     """
-    Performs data augmentation by creating a scaled version of the training features
-    and appending it to the original training data.
+        Fits a StandardScaler on the training data and creates a modified
+        version of the dataset by subtracting (mean / 100) from each feature.
 
-    Parameters:
-        X_train (DataFrame): Original training features
-        y_train (Series): Original training labels
+        The function then vertically concatenates:
+            - the original training features
+            - the modified feature set
 
-    Returns:
-        X_augmented (DataFrame): Original + scaled features
-        y_augmented (Series): Original + duplicated labels
+        To maintain alignment, the training labels are also duplicated
+        and concatenated accordingly.
+
+        Parameters:
+            X_train (DataFrame): Original training features
+            y_train (Series): Original training labels
+
+        Returns:
+            X_scaled (DataFrame): Combined dataset containing original and
+                                  transformed feature rows.
+            y_scaled (Series): Corresponding duplicated labels.
+
     """
 
     scaler = StandardScaler()
 
-    X_scaled = scaler.fit_transform(X_train)
+    X_scaled = scaler.fit(X_train)
+
+    X_scaled = X_train.values - X_scaled.mean_/100
+
     X_scaled = pd.DataFrame(X_scaled, columns=X_train.columns)
 
-    X_augmented = pd.concat([X_train, X_scaled], axis=0).reset_index(drop=True)
+    X_augmented = pd.concat([X_train, X_scaled], axis=0, ignore_index=True)
 
-    y_augmented = pd.concat([y_train, y_train], axis=0).reset_index(drop=True)
+    y_augmented = pd.concat([y_train, y_train], ignore_index=True)
 
     return X_augmented, y_augmented
